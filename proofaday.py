3dd28317f390bd14fce6ca55324851ca9ba38a70
whonore <wolfhonore@gmail.com>
2019-08-25 14:55:20 -0700

    Parse more math

--- proofaday.py
+++ proofaday.py
@@ -3,11 +3,15 @@ import re
 import requests
 from bs4 import BeautifulSoup as BS
 from pylatexenc.latex2text import (
-    LatexNodes2Text as L2T,
-    get_default_latex_context_db,
+    LatexNodes2Text,
+    get_default_latex_context_db as l2t_ctx,
     MacroTextSpec
 )
-# from pylatexenc.macrospec import
+from pylatexenc.latexwalker import (
+    get_default_latex_context_db as lwalk_ctx,
+    LatexWalker
+)
+from pylatexenc.macrospec import std_macro
 
 URL = "https://proofwiki.org/wiki/"
 RANDOM = "Special:Random"
@@ -22,6 +26,44 @@ tmps = (
 )
 tmp = tmps[-1]
 
+MACROS = [
+    MacroTextSpec(name, simplify_repl=repl) for name, repl in (
+        ('R', '\N{DOUBLE-STRUCK CAPITAL R}'),
+        ('Z', '\N{DOUBLE-STRUCK CAPITAL Z}'),
+        ('N', '\N{DOUBLE-STRUCK CAPITAL N}'),
+        ('lfloor', '\N{LEFT FLOOR}'), ('rfloor', '\N{RIGHT FLOOR}'),
+        ('lceil', '\N{LEFT CEILING}'), ('rceil', '\N{RIGHT CEILING}'),
+        ('blacksquare', '\N{BLACK SQUARE}'),
+        ('in', '\N{ELEMENT OF} '),
+        ('dfrac', '%s/%s')
+    )
+]
+MACRO_ARGS = [std_macro('dfrac', False, 2)]
+
+CTX = l2t_ctx()
+CTX.add_context_category('mathjax', macros=MACROS, prepend=True)
+ARGS = lwalk_ctx()
+ARGS.add_context_category('mathjax', macros=MACRO_ARGS, prepend=True)
+
+L2T = LatexNodes2Text(CTX, math_mode='text')
+
+
+def to_text(tag):
+    if tag.name == 'p':
+        return tag.get_text()
+    elif tag.name == 'dl':
+        return "\\qquad{}\n".format(tag.get_text())
+    elif tag.name == 'table':
+        txt = []
+        for row in tag.find_all('tr'):
+            row_txt = []
+            for el in row.find_all('td'):
+                el_txt = el.get_text().strip()
+                if el_txt != '':
+                    row_txt.append(el_txt)
+            txt.append(r'\qquad' + r'\ '.join(row_txt))
+        return r'\\'.join(txt)
+
 def get_proof(name=None):
     if name is None:
         name = RANDOM
@@ -34,34 +76,21 @@ def get_proof(name=None):
         proof = body.find('span', id='Proof')
         # TODO: strip text after proof end
         if title is not None and theorem is not None and proof is not None:
-            theorem_body = theorem.parent.find_next_siblings('p')
-            proof_body = proof.parent.find_next_siblings('p')
+            tags = ['p', 'dl', 'table']
+            theorem_body = theorem.parent.find_next_siblings(tags)
+            proof_body = proof.parent.find_next_siblings(tags)
             theorem_body = theorem_body[:-len(proof_body)]
+
             return (title.get_text(),
-                    ''.join([x.get_text() for x in theorem_body]).strip(),
-                    ''.join([x.get_text() for x in proof_body]).strip())
+                    ''.join(to_text(tag) for tag in theorem_body).strip(),
+                    ''.join(to_text(tag) for tag in proof_body).strip())
         elif name != RANDOM:
             return None
     return None
-
-MACROS = [
-    MacroTextSpec(*macro) for macro in [
-        ('R', 'ℝ'), ('Z', 'ℤ'), ('N', 'ℕ'),
-        ('lfloor', '⌊'), ('rfloor', '⌋'), ('lceil', '⌈'), ('rceil', '⌉'),
-        ('blacksquare', '⬛'),
-        ('in', '∈ ')
-    ]
-]
-
-CTX = get_default_latex_context_db()
-CTX.add_context_category('mathjax', macros=MACROS, prepend=True)
-
-def asciify(math):
-    return L2T(CTX, math_mode='text').latex_to_text(math)
 
 if __name__ == '__main__':
     ret = get_proof(tmp)
     if ret is not None:
         title, theorem, proof = ret
-        theorem, proof = asciify(theorem), asciify(proof)
-        print(f"Theorem {title}\n{'=' * len('Theorem ' + title)}\n{theorem}\n\nProof:\n{proof}")
+        theorem, proof = L2T.latex_to_text(theorem, latex_context=ARGS), L2T.latex_to_text(proof,latex_context=ARGS)
+        print("{}\n{}\n{}\n\nProof:\n{}".format(title, '=' * len(title), theorem, proof))
