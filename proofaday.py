a85a856f6daaa5d60bb5a2f3198a214406bfa68f
whonore <wolf.honore@sifive.com>
2019-08-26 14:47:03 -0700

    Add more arguments

--- proofaday.py
+++ proofaday.py
@@ -3,6 +3,7 @@ import socket
 import socketserver
 import sys
 import threading
+from argparse import ArgumentParser
 from queue import Queue
 
 import requests
@@ -13,10 +14,10 @@ from syms import latex_to_text
 URL = "https://proofwiki.org/wiki/"
 RANDOM = "Special:Random"
 MAX_RETRY = 5
-MAX_PROOFS = 10
+NPREFETCH = 10
 PROOF_END = re.compile(r"blacksquare")
 PORT = 48484
-HOST = 'localhost'
+HOST = "localhost"
 TEST_PAGES = (
     "Union_of_Left-Total_Relations_is_Left-Total",
     "Frattini's_Argument",
@@ -27,6 +28,7 @@ TEST_PAGES = (
     "Inverse_of_Algebraic_Structure_Isomorphism_is_Isomorphism/General_Result",
     "If Every Element Pseudoprime is Prime then Way Below Relation is Multiplicative",
 )
+
 
 def node_to_text(node):
     if node.name == "p":
@@ -56,7 +58,7 @@ def get_proof(name=None):
         proof = body.find("span", id="Proof")
 
         if title is not None and theorem is not None and proof is not None:
-            tags = ["p", "dl", "table"]
+            tags = ("p", "dl", "table")
             theorem_body = theorem.parent.find_next_siblings(tags)
             proof_body = proof.parent.find_next_siblings(tags)
             theorem_body = theorem_body[: -len(proof_body)]
@@ -86,19 +88,24 @@ def get_proof(name=None):
 def format_proof(title, theorem, proof):
     return "{}\n{}\n{}\n\nProof:\n{}".format(title, "=" * len(title), theorem, proof)
 
+
 class ProofHandler(socketserver.BaseRequestHandler):
     def handle(self):
         msg, sock = self.request
-        if msg != b'':
-            proof = self.server.fetch_proof(str(msg, 'utf8'))
+        if msg != b"":
+            proof = self.server.fetch_proof(str(msg, "utf8"))
         else:
             proof = self.server.queue.get()
-        sock.sendto(bytes(proof, 'utf8') if proof is not None else b'', self.client_address)
+        sock.sendto(
+            bytes(proof, "utf8") if proof is not None else b"", self.client_address
+        )
+
 
 class ProofServer(socketserver.UDPServer):
-    def __init__(self, port):
+    def __init__(self, port, limit, nprefetch, **kwargs):
         super().__init__((HOST, port), ProofHandler)
-        self.queue = Queue(maxsize=MAX_PROOFS)
+        self.queue = Queue(maxsize=nprefetch)
+        self.limit = limit
         self.fetcher = threading.Thread(target=self.fetch_proofs)
         self.fetcher.start()
 
@@ -120,33 +127,49 @@ class ProofServer(socketserver.UDPServer):
     def fetch_proofs(self):
         while True:
             proof = self.fetch_proof()
-            if proof is not None:
+            if proof is not None and (
+                self.limit is None or len(proof.split("\n")) <= self.limit
+            ):
                 self.queue.put(proof)
 
-def start_server():
-    with ProofServer(PORT) as server:
+
+def start_server(*args, **kwargs):
+    with ProofServer(*args, **kwargs) as server:
         server.serve_forever()
 
-def query_server(name):
+
+def query_server(port, name):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-        sock.sendto(bytes(name, 'utf8') if name is not None else b'', (HOST, PORT))
-        return str(sock.recv(4096), 'utf8')
+        if name is not None:
+            msg = bytes(name, "utf8")
+        else:
+            msg = b""
+        sock.sendto(msg, (HOST, port))
+        return str(sock.recv(4096), "utf8")
+
+
+def pos(arg):
+    arg = int(arg)
+    if arg <= 0:
+        raise ValueError("not positive")
+    return arg
+
 
 if __name__ == "__main__":
-    try:
-        idx = sys.argv.index("-d")
-        sys.argv.pop(idx)
-        debug = True
-    except ValueError:
-        debug = False
-    try:
-        name = sys.argv[1]
-        if name[0].isdigit():
-            name = TEST_PAGES[int(name)]
-    except IndexError:
-        name = None
+    parser = ArgumentParser(description="Fetch a random proof")
+    parser.add_argument("name", nargs="?", default=None)
+    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-l", "--limit", type=pos, default=None)
+    parser.add_argument(
+        "-n", "--num-prefetch-proofs", dest="nprefetch", type=pos, default=NPREFETCH
+    )
+    parser.add_argument("-p", "--port", type=int, default=PORT)
+    args = parser.parse_args()
+
+    if args.name is not None and args.name[0].isdigit():
+        args.name = TEST_PAGES[int(args.name)]
 
     try:
-        start_server()
+        start_server(**vars(args))
     except OSError:
-        print(query_server(name))
+        print(query_server(args.port, args.name))
