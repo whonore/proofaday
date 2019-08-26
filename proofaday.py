1da75f896719d222ac9cf3c129ff7d257e5fbb70
whonore <wolfhonore@gmail.com>
2019-08-25 22:02:04 -0700

    Add many more symbols

--- proofaday.py
+++ proofaday.py
@@ -1,87 +1,65 @@
 import re
+import sys
 
 import requests
 from bs4 import BeautifulSoup as BS
-from pylatexenc.latex2text import (
-    LatexNodes2Text,
-    get_default_latex_context_db as l2t_ctx,
-    MacroTextSpec,
-)
-from pylatexenc.latexwalker import (
-    get_default_latex_context_db as lwalk_ctx,
-    LatexWalker,
-)
-from pylatexenc.macrospec import std_macro
+
+from syms import latex_to_text
 
 URL = "https://proofwiki.org/wiki/"
 RANDOM = "Special:Random"
 MAX_RETRY = 5
-PROOF_END = re.compile(r'blacksquare')
-tmps = (
-    None,
-    "Union_of_Left-Total_Relations_is_Left-Total",  # missing
-    "Frattini's_Argument",  # missing
-    "Floor_of_Ceiling_is_Ceiling",  # missing some text
+PROOF_END = re.compile(r"blacksquare")
+TEST_PAGES = (
+    "Union_of_Left-Total_Relations_is_Left-Total",
+    "Frattini's_Argument",
+    "Floor_of_Ceiling_is_Ceiling",
     "Existence_of_Unique_Subsemigroup_Generated_by_Subset",
     "Largest_Rectangle_with_Given_Perimeter_is_Square",
+    "Event_Independence_is_Symmetric",
+    "Inverse_of_Algebraic_Structure_Isomorphism_is_Isomorphism/General_Result",
+    "If Every Element Pseudoprime is Prime then Way Below Relation is Multiplicative",
 )
-tmp = tmps[-1]
-
-MACROS = [
-    MacroTextSpec(name, simplify_repl=repl)
-    for name, repl in (
-        ("R", "\N{DOUBLE-STRUCK CAPITAL R}"),
-        ("Z", "\N{DOUBLE-STRUCK CAPITAL Z}"),
-        ("N", "\N{DOUBLE-STRUCK CAPITAL N}"),
-        ("lfloor", "\N{LEFT FLOOR}"),
-        ("rfloor", "\N{RIGHT FLOOR}"),
-        ("lceil", "\N{LEFT CEILING}"),
-        ("rceil", "\N{RIGHT CEILING}"),
-        ("blacksquare", "\N{BLACK SQUARE}"),
-        ("in", "\N{ELEMENT OF} "),
-        ("dfrac", "%s/%s"),
-    )
-]
-MACRO_ARGS = [std_macro("dfrac", False, 2)]
-
-CTX = l2t_ctx()
-CTX.add_context_category("mathjax", macros=MACROS, prepend=True)
-ARGS = lwalk_ctx()
-ARGS.add_context_category("mathjax", macros=MACRO_ARGS, prepend=True)
-
-L2T = LatexNodes2Text(CTX, math_mode="text")
 
 
-def to_text(tag):
-    if tag.name == "p":
-        return tag.get_text()
-    elif tag.name == "dl":
-        return "\\qquad{}\n".format(tag.get_text())
-    elif tag.name == "table":
+def node_to_text(node):
+    if node.name == "p":
+        return node.get_text()
+    elif node.name == "dl":
+        return "\\qquad{}\n".format(node.get_text())
+    elif node.name == "table":
         txt = []
-        for row in tag.find_all("tr"):
+        for row in node.find_all("tr"):
             row_txt = []
             for el in row.find_all("td"):
                 row_txt += list(el.stripped_strings)
             txt.append(r"\qquad" + r"\ ".join(row_txt))
-        return r"\\".join(txt)
+        return r"\\".join(txt) + "\n"
 
 
 def get_proof(name=None):
     if name is None:
         name = RANDOM
     url = URL + name
+
     for _ in range(MAX_RETRY):
         html = BS(requests.get(url).text, "html.parser")
         title = html.find("h1", id="firstHeading")
         body = html.find("div", id="bodyContent")
         theorem = body.find("span", id="Theorem")
         proof = body.find("span", id="Proof")
+
         if title is not None and theorem is not None and proof is not None:
             tags = ["p", "dl", "table"]
             theorem_body = theorem.parent.find_next_siblings(tags)
             proof_body = proof.parent.find_next_siblings(tags)
             theorem_body = theorem_body[: -len(proof_body)]
+
+            if theorem_body == [] or proof_body == []:
+                if name != RANDOM:
+                    break
+                else:
+                    continue
 
             # Strip text after proof end
             for idx, node in enumerate(proof_body):
@@ -91,25 +69,42 @@ def get_proof(name=None):
 
             return (
                 title.get_text(),
-                "".join(to_text(tag) for tag in theorem_body).strip(),
-                "".join(to_text(tag) for tag in proof_body).strip(),
+                "".join(node_to_text(node) for node in theorem_body).strip(),
+                "".join(node_to_text(node) for node in proof_body).strip(),
             )
         elif name != RANDOM:
-            return None
+            break
     return None
+
+
+def format_proof(title, theorem, proof):
+    return "{}\n{}\n{}\n\nProof:\n{}".format(title, "=" * len(title), theorem, proof)
 
 
 if __name__ == "__main__":
     try:
-        ret = get_proof(tmp)
+        idx = sys.argv.index("-d")
+        sys.argv.pop(idx)
+        debug = True
+    except ValueError:
+        debug = False
+    try:
+        name = sys.argv[1]
+        if name[0].isdigit():
+            name = TEST_PAGES[int(name)]
+    except IndexError:
+        name = None
+
+    try:
+        title, theorem, proof = get_proof(name)
+    except TypeError:
+        sys.exit(1)
     except ConnectionResetError:
-        ret = None
-    if ret is not None:
-        title, theorem, proof = ret
-        theorem, proof = (
-            L2T.latex_to_text(theorem, latex_context=ARGS),
-            L2T.latex_to_text(proof, latex_context=ARGS),
-        )
-        print(
-            "{}\n{}\n{}\n\nProof:\n{}".format(title, "=" * len(title), theorem, proof)
-        )
+        sys.exit(1)
+
+    if debug:
+        print("===DEBUG===")
+        print(format_proof(title, theorem, proof))
+        print("===DEBUG===\n")
+
+    print(format_proof(title, latex_to_text(theorem), latex_to_text(proof)))
