e9bf7d0f3fe0e37448bfd199d69aedbd96238514
whonore <wolfhonore@gmail.com>
2020-04-02 15:23:46 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(dir / "README.md", "r") as f:
 
 setup(
     name="proofaday",
-    version="0.2.2",
+    version="0.2.3",
     author="Wolf Honoré",
     author_email="wolfhonore@gmail.com",
     description="Print random proofs from ProofWiki",
